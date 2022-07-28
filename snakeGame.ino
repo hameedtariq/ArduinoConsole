92b6e97dce6f4e7cd596e17d2a366ffd79d65402
Muhammad Hameed <muhammadhameed105@gmail.com>
2022-07-28 22:37:37 +0500

    Snake file

---
+++ snakeGame.ino
@@ -0,0 +1,243 @@
+#include "LedControl.h"
+#include <Vector.h>
+
+static const short CLK = 4;   // clock for LED matrix
+static const short CS  = 2;  // chip-select for LED matrix
+static const short DIN = 3;
+static const short buttonDownPin = 7;
+static const short buttonUpPin = 12;
+static const short buttonLeftPin = 8;
+static const short buttonRightPin = 9;
+
+const short intensity = 3;
+
+const short messageSpeed = 5;
+
+LedControl matrix(DIN,CLK, CS, 1);
+
+struct COORD {
+  short X;
+  short Y;
+
+  COORD(short Xin, short Yin){
+    X = Xin;
+    Y = Yin;
+  }
+};
+
+
+class Food {
+private:
+  COORD pos;
+public:
+  Food(): pos(0,0) {genFood();};
+  void genFood(){
+    pos.X = random(0,8);
+    pos.Y = random(0,8);
+  }
+  COORD getFoodPos() {
+    return pos;
+  }
+};
+
+
+class Snake {
+  private:
+    COORD pos;
+    int len;
+    char direction;
+    Vector<COORD> body;
+  public:
+    Vector<COORD> getSnakeBody() {
+      return body;
+    }
+    void grow(){
+      len++;
+    }
+    COORD getPos(){
+      return pos;
+    }
+    void moveSnake() {
+      switch(direction) {
+        case 'L':
+          pos.Y -= 1;
+          break;
+        case 'R':
+          pos.Y += 1;
+          break;
+        case 'D':
+          pos.X += 1;
+          break;
+        case 'U':
+          pos.X -= 1;
+          break;       
+      }
+
+      if(pos.X < 0){
+        pos.X = 7;
+      }else if(pos.X > 7){
+        pos.X = 0;
+      }
+
+      if(pos.Y < 0){
+        pos.Y = 7;
+      }else if(pos.Y > 7){
+        pos.Y = 0;
+      }
+
+      body.push_back(pos);
+      if(body.size() > len){
+        body.remove(0);
+      }
+    }
+    void setDirection(char d){
+      direction = d;
+    }
+    Snake(): pos(0,0),direction('N'),len(1) {
+        body.push_back(pos);
+    };
+    bool eatenFood(COORD foodPos) {
+      if(pos.X == foodPos.X && pos.Y == foodPos.Y){
+        return true;
+      }
+      return false;
+    }
+};
+
+
+
+
+
+
+Snake snake;
+Food food;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);  
+  pinMode(buttonDownPin, INPUT_PULLUP);
+  pinMode(buttonUpPin, INPUT_PULLUP);
+  pinMode(buttonRightPin, INPUT_PULLUP);
+  pinMode(buttonLeftPin, INPUT_PULLUP);
+  matrix.shutdown(0, false);
+  matrix.setIntensity(0, intensity);
+  matrix.clearDisplay(0);
+  randomSeed(analogRead(1));
+  showSnakeMessage();
+}
+
+void renderBoard() {
+  COORD snakePos = snake.getPos();
+  COORD foodPos = food.getFoodPos();
+  Vector<COORD> snakeBody = snake.getSnakeBody();
+  for(int i =0; i <8; i++){
+    for(int j = 0; j< 8; j++){
+      if(i == snakePos.X && j == snakePos.Y){
+        matrix.setLed(0, i, j, 1);
+      }
+      else if (i == foodPos.X && j == foodPos.Y){
+        matrix.setLed(0, i, j, 1);
+      }
+      else {
+        bool bodyPart = false;
+        for(int k =0; k < snakeBody.size(); k++){
+          if(i == snakeBody[k].X && j == snakeBody[k].Y){
+            matrix.setLed(0, i, j, 1);
+            bodyPart = true;
+          }
+        }
+        if(!bodyPart) {
+          matrix.setLed(0, i, j, 0);
+        }
+        
+      }
+    }
+  }
+}
+
+int downPress = 0,upPress = 0,leftPress = 0,rightPress = 0;
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  downPress = digitalRead(buttonDownPin);
+  upPress = digitalRead(buttonUpPin);
+  leftPress = digitalRead(buttonLeftPin);
+  rightPress = digitalRead(buttonRightPin);
+  Serial.print("Left");
+  Serial.println(leftPress);
+  Serial.print("rightPress");
+  Serial.println(rightPress);
+  Serial.print("upPress");
+  Serial.println(upPress);
+  Serial.print("downPress");
+  Serial.println(downPress);
+  if(downPress){
+    snake.setDirection('D');
+  }
+  else if(upPress){
+     snake.setDirection('U');
+  }
+  else if(leftPress){
+     snake.setDirection('L');
+  }
+  else if(rightPress){
+     snake.setDirection('R');
+  }
+  snake.moveSnake();
+  if(snake.eatenFood(food.getFoodPos())){
+    food.genFood();
+    snake.grow();
+  }
+  renderBoard();
+  
+  delay(100);
+  
+}
+
+
+const PROGMEM bool snakeMessage[8][56] = {
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+};
+const PROGMEM bool gameOverMessage[8][90] = {
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+};
+
+const PROGMEM bool scoreMessage[8][58] = {
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+};
+
+void showSnakeMessage() {
+  [&] {
+    for (int d = 0; d < sizeof(snakeMessage[0]) - 7; d++) {
+      for (int col = 0; col < 8; col++) {
+        delay(messageSpeed);
+        for (int row = 0; row < 8; row++) {
+          // this reads the byte from the PROGMEM and displays it on the screen
+          matrix.setLed(0, row, col, pgm_read_byte(&(snakeMessage[row][col + d])));
+        }
+      }
+    }
+  }();
+  matrix.clearDisplay(0);
+}
